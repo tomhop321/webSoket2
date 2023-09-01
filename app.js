0fa68e0245592fe1a29d76dcc62805ad052b8b02
tomhop321 <85928808+tomhop321@users.noreply.github.com>
2023-09-01 13:32:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,4 +7,9 @@ console.log("New client connected!");
 ws.on("close", () => {
 });
 console.log("Client has disconnected!");
+      ws.send('This is a message from server, connection is established');
+    //receive the message from client on Event: 'message'
+    ws.on('message', (msg) => {
+      if(msg.toString('ascii') == 2){ws.send("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")}
+        console.log(msg.toString('ascii'));
 });
