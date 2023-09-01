bba568d9a4286bef558ba66d4363d4c58e3026fd
tomhop321 <85928808+tomhop321@users.noreply.github.com>
2023-09-01 09:38:53 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+const WebSocket = require("ws");
+const wss = new WebSocket.Server({ port: 8082 });
+wss.on("connection", ws => {
+console.log("New client connected!");
+ws.on("close", () => {
+});
+console.log("Client has disconnected!");
+});
