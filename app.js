0b09095ddad519d0913c5a94bf38f6545004f2d1
tomhop321 <85928808+tomhop321@users.noreply.github.com>
2023-09-01 13:38:44 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
+const http = require('http');
+const websocket = require('ws');
 
+const server = http.createServer((req, res) => {
+    res.end("I am connected");
+});
+const wss = new websocket.Server({ server });
 
-const WebSocket = require("ws");
-const wss = new WebSocket.Server({port: 3000});
-wss.on("connection", ws => {
-console.log("New client connected!");
-ws.on("close", () => {
+wss.on('headers', (headers, req) => {
+    //console.log(headers); Not logging the header anymore
+});
 
-console.log("Client has disconnected!");
-      ws.send('This is a message from server, connection is established');
+//Event: 'connection'
+wss.on('connection', (ws, req) => {
+    ws.send('This is a message from server, connection is established');
     //receive the message from client on Event: 'message'
     ws.on('message', (msg) => {
-      if(msg.toString('ascii') == 2){ws.send("eeeeeeeeee")}
-       // console.log(msg.toString('ascii'));
+      if(msg.toString('ascii') == 2){ws.send("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")}
+        console.log(msg.toString('ascii'));
+    });
 });
-});
+
+server.listen(3000);
